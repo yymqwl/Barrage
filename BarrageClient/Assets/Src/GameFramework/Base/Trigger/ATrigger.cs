f9cf0be919e81b295ced286a8b0b2fb9943dab45
Zsy <25689328@qq.com>
2020-04-29 20:54:32 +0800

    添加一波更新

---
+++ BarrageClient/Assets/Src/GameFramework/Base/Trigger/ATrigger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GameFramework
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T0">psend</typeparam>
+    /// <typeparam name="T1">param</typeparam>
+    /// <returns></returns>
+
+    
+    public abstract class ATrigger :CObject
+    {
+        int m_TriggerTimes;
+
+        public int TriggerTimes //大于0判断是否触发完毕
+        {
+            get { return m_TriggerTimes; }
+            set { m_TriggerTimes = value; }
+        }
+
+        /// <summary>
+        /// 是否还能触发
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool HasTrigger()
+        {
+            return TriggerTimes > 0;
+        }
+
+        public override void Init()
+        {
+            TriggerTimes = 1;
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+
+        public virtual void Update(float elapsedTime)
+        {
+
+        }
+    }
+}
---
+++ BarrageClient/Assets/Src/GameFramework/Base/Trigger/TimeTrigger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+    public class TimeTrigger :ATrigger
+    {
+
+        Action<object, object> m_Act;
+        float m_StartTime;//开始时间
+        float m_TrgTime;//触发时间
+        bool m_IsStart;//是否已开始
+        public void Start(float TrgTime, Action<object, object>  act)
+        {
+            m_StartTime = 0 ;
+            m_TrgTime = TrgTime;
+            m_IsStart = true;
+            m_Act = act;
+            TriggerTimes = 1;
+        }
+        public override void Init()
+        {
+            base.Init();
+            m_StartTime = 0;
+            m_TrgTime = 0;
+            m_IsStart = false;
+        }
+        public static TimeTrigger Create(float TrgTime, Action<object, object> act)
+        {
+            TimeTrigger tt = new TimeTrigger();
+            tt.Start(TrgTime,act);
+            return tt;
+        }
+
+
+
+        public override void Update(float elapsedTime)
+        {
+            if (!m_IsStart )
+            {
+                return;
+            }
+            if (!HasTrigger())//没有触发次数
+            {
+                return;
+            }
+            m_StartTime +=elapsedTime;
+            if (m_StartTime >=m_TrgTime)
+            {
+                m_Act.InvokeGracefully(m_StartTime,null);
+                TriggerTimes--;
+            }
+        }
+
+
+    }
+}
